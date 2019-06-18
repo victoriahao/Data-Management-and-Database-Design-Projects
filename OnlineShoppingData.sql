a84a31311c49d1bd68cb775fe5eb9948cbc58de7
Meng-Wei Hao <42991208+victoriahao@users.noreply.github.com>
2019-06-18 14:57:19 -0700

    Add files via upload

---
+++ OnlineShoppingData.sql
@@ -0,0 +1,548 @@
+/*
+Group 8
+
+
+Database: OnlineShoppingMall
+*/
+
+
+INSERT User_Account
+VALUES 
+(1, 'James', 'JS12345', '2017-11-17', 5),
+(2, 'Steve', 'SUL101', '2017-11-25', 1),
+(3, 'Amy', 'MM900', '2017-12-10', 1),
+(4, 'Mark', 'VST246', '2017-12-18', 1),
+(5, 'Nicola', 'YY765', '2018-01-07', 2),
+(6, 'Elizabeth', 'zoo892', '2018-2-13', 3),
+(7, 'Susan', 'WEG753', '2018-3-5', 2),
+(8, 'Kelly', 'cici818', '2018-4-6', 4),
+(9, 'David', 'UI799', '2018-5-19', 2),
+(10, 'Lucas', 'am6754','2018-05-25', 2)
+
+select * from User_Account; 
+
+INSERT User_Session
+VALUES 
+(1, '24.18.105.231', '10', '2017-11-17', 1),
+(2, '24.34.1.102', '8', '2017-11-25', 2),
+(3, '24.49.216.172', '5', '2017-12-10', 3),
+(4, '24.73.39.89', '22', '2017-12-18', 4),
+(5, '24.18.105.231', '15', '2017-12-24', 1),
+(6, '24.18.105.231', '20', '2017-12-31', 1),
+(7, '24.79.169.21', '17', '2018-01-07', 5),
+(8, '24.89.107.79', '15', '2018-02-13', 6),
+(9, '24.18.105.231', '27', '2018-03-01', 1),
+(10, '24.62.71.57', '35', '2018-03-05', 7),
+(11, '24.89.107.79', '14', '2018-03-30', 6),
+(12, '24.36.18.300', '18', '2018-04-06', 8),
+(13, '24.79.169.21', '20', '2018-04-10', 5),
+(14, '24.36.18.300', '26', '2018-04-13', 8),
+(15, '24.89.107.79', '24', '2018-04-17', 6),
+(16, '24.36.18.300', '17', '2018-04-27', 8),
+(17, '24.36.18.300', '20', '2018-05-02', 8),
+(18, '24.62.71.57', '26', '2018-05-06', 7),
+(19, '24.18.105.231', '16', '2018-05-10', 1),
+(20, '24.78.156.64', '23', '2018-05-19', 9),
+(21, '24.25.257.31', '130', '2018-05-25', 10),
+(22, '24.78.156.64', '27', '2018-06-20', 9),
+(23, '24.25.257.31', '28', '2018-07-08', 10);
+
+select * from User_Session;
+
+
+INSERT InventoryCatalog
+VALUES (1, 'Women Clothing'),
+	   (2, 'Men Clothing'),
+	   (3, 'Kids Clothing'),
+	   (4, 'Women Shoes'),
+	   (5, 'Men Shoes'),
+	   (6, 'Kids Shoes'),
+	   (7, 'Accessories'),
+	   (8, 'Furniture'),
+	   (9, 'Decoration'),
+	   (10, 'Electronic Products'),
+	   (11, 'Kitchen Products');
+
+select * from InventoryCatalog;
+
+INSERT Vendor
+VALUES (1, 'Express', '2080 Park Centre Dr'),
+	   (2, 'Wearhouse', '8950 W Charless Blvd'),
+	   (3, 'DressBarn', '8740 W Charless Blvd'),
+	   (4, 'Madewell', '1985 Festival Plaza Dr'),
+	   (5, 'Vans', '410 Meadows St'),
+	   (6, 'Journeys', '775 S 8th Ave'),
+	   (7, 'Pottery Home D¨¦cor', '6710 N Russell Rd'),
+	   (8, 'Ashley Store', '3800 Town Center Dr'),
+	   (9, 'Furniture Place', '205 Rainbow Blvd'),
+	   (10, 'Fly Electronics', '683 Terry Ave'),
+	   (11, 'Cap Cookhouse', '4300 Westlake Dr');
+
+SELECT * FROM Vendor; 
+
+
+
+INSERT Inventory_Item
+VALUES
+(50, 'Soutach,e Lace Dress', 100, 1, 4),
+(28, 'North Pole Jackets',	200, 2,	6),
+(90, 'Peek Agnes Sweater',	150, 3,	3),
+(65, 'Velvet Leggings',	300, 1,	2),
+(120,'Tate Sweater',350,2,2),
+(70,'Reindeer Sweater Dress',200,3,	1),
+(15,'Cat Beanie',300,7,	6),
+(36,'Osito Gloves',	400,7,1),
+(59,'Furry Infinity Scarf',	200,7,3),
+(40, 'J&M Boots',350,5,	5),
+(70,'Star Heels',100,4,	4),
+(30,'Vans Sport shoes',100,	6,	5),
+(35,'Chairs',100,8,9),
+(80,'Computer Desk',150,8,9),
+(38,'Mosaic Mat',30,9,8),
+(50	,'Snowflake lamp',80,9,	7),
+(29,'Headphone'	,50	,10	,10),
+(18,'Fast USB Drive',50	,10	,10),
+(30,'Lilia Dish',100,11,11),
+(80,'Stainless Steel Cookware Set',	80,	11,	11)
+
+SELECT * FROM Inventory_Item
+
+INSERT Shopping_Cart
+VALUES
+(1,	1,	115, 1),
+(2,	1,	268, 2),
+(3,	1,	142, 3),
+(4,	1,	110, 4),
+(5,	1,	140, 5),
+(6,	1,	80,	6),
+(7,	1,	50,	7),
+(8,	1,	70,	8),
+(9,	1,	40,	9),
+(10, 1,	76,	10)
+
+select * from Shopping_Cart;
+
+INSERT Cart_Detail
+VALUES
+(1,	1,	1,	'2017-11-17',	1),
+(2,	4,	1,	'2017-11-17',	1),
+(3,	2,	1,	'2017-11-25',	2),
+(4,	5,	2,	'2017-11-25',	2),
+(5,	8,	2,	'2017-12-10',	3),
+(6,	11,	1,	'2017-12-10',	3),
+(7,	19,	1,	'2017-12-24',	4),
+(8,	20,	1,	'2017-12-24',	4),
+(9,	13,	4,	'2018-01-07',	5),
+(10,14,	1,	'2018-02-13',	6),
+(11,16,	1,	'2018-03-05',	7),
+(12,6,	1,	'2018-04-06',	8),
+(13,10,	1,	'2018-05-19',	9),
+(14,15,	2,	'2018-05-25',	10)
+
+select * from Cart_Detail;
+
+
+
+/* INSERT INTO ORDERHEADER*/ 
+INSERT INTO OrderHeader
+VALUES
+('2018-11-01',207.5,'p',1,0,20.75);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-02',500,'u',1,0,50);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-02',500,'u',2,55,50);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-07',10,'u',2,0,1);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-07',39,'p',4,0.39,3.9);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-07',10,'u',4,0,1);
+
+DELETE FROM OrderHeader
+WHERE OrderID IN (8,9,10,11)
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-08',1000,'u',4,55,100);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-08',4999,'p',5,99,499.9);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-10',60,'p',3,0,6);
+
+INSERT INTO OrderHeader
+VALUES
+('2018-11-16',999,'u',6,9,99.9);
+
+/*INSERT INTO CreditCard*/
+
+DROP TABLE OrderHeader
+DROP TABLE OrderItem
+DROP TABLE OrderItemHistory
+DROP TABLE Payment
+DROP TABLE CreditCard
+
+
+INSERT INTO CreditCard
+VALUES
+(1000000000000000,'James','2020-1-31',1)
+
+INSERT INTO CreditCard
+VALUES
+(1000000000000006,'James','2025-8-31',1)
+
+INSERT INTO CreditCard
+VALUES
+(1000000645000009,'Steve','2020-8-31',2)
+
+INSERT INTO CreditCard
+VALUES
+(1004900645000009,'Amy','2021-3-31',3)
+
+INSERT INTO CreditCard
+VALUES
+(1000000645004446,'Amy','2019-8-31',3)
+
+INSERT INTO CreditCard
+VALUES
+(3890000645004446,'Elizabeth','2021-7-31',6)
+
+INSERT INTO CreditCard
+VALUES
+(3890000645007777,'Elizabeth','2023-4-30',6)
+
+INSERT INTO CreditCard
+VALUES
+(3890000645055555,'Susan','2024-9-30',7)
+
+INSERT INTO CreditCard
+VALUES
+(3890000645007367,'Susan','2023-11-30',7)
+
+INSERT INTO CreditCard
+VALUES
+(3890000645447367,'David','2020-11-30',9)
+
+INSERT INTO CreditCard
+VALUES
+(1234000645447367,'Mark','2020-11-30',4)
+/*INSERT INTO Payment
+ * PaymentID INT IDENTITY,
+	TotalAmount decimal(10,2) NOT NULL,
+	Status VARCHAR(255) NOT NULL,
+	PayTime TIMESTAMP,
+	CardID BIGINT,
+	OrderID INT,*/
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(20,'Succeed',1000000000000000,1)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(30,'Succeed',1000000000000000,1)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(50,'Succeed',1000000000000000,1)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(100,'Succeed',1000000000000000,1)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(28.25,'Succeed',1000000000000000,1)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(42.51,'Succeed',1234000645447367,5)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(10,'Succeed',1004900645000009,9)
+
+
+INSERT INTO Payment
+(TotalAmount,Status,CardID,OrderID)
+VALUES
+(6,'Succeed',1004900645000009,9)
+
+/*Insert into OrderItem
+ * 	OrderItemID INT IDENTITY,
+	ItemID INT NOT NULL,
+	qty INT NOT NULL,
+	OrderID INT NOT NULL,
+	PRIMARY KEY (OrderItemID),
+	FOREIGN KEY (OrderID) REFERENCES OrderHeader(OrderID),
+	FOREIGN KEY (ItemID) REFERENCES Inventory_Item(item_id)
+*/
+INSERT INTO OrderItem
+VALUES
+(2,5,1)
+
+INSERT INTO OrderItem
+VALUES
+(4,1,1)
+
+INSERT INTO OrderItem
+VALUES
+(2,2,1)
+
+INSERT INTO OrderItem
+VALUES
+(5,5,1)
+
+INSERT INTO OrderItem
+VALUES
+(1,5,1)
+
+INSERT INTO OrderItem
+VALUES
+(7,5,1)
+
+
+INSERT INTO OrderItem
+VALUES
+(7,5,2)
+
+
+INSERT INTO OrderItem
+VALUES
+(1,6,3)
+
+INSERT INTO OrderItem
+VALUES
+(4,1,3)
+
+INSERT INTO OrderItem
+VALUES
+(2,4,3)
+
+
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (1, 1, 1, 10,8,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (2, 1, 3, 8,8,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (3, 5, 1, 10,8,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (4, 1, 1, 7,6,'ok');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (5, 2, 4, 9,4,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (6, 1, 5, 1,8,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (7, 2, 1, 8,7,'bad');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (8, 1, 8, 2,8,'nice');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (9, 5, 1, 7,6,'bad');
+INSERT INTO Customer_Service (Cus_Ser_ID, User_ID, Item_ID, Staff_ID,Session_ID,Notes)
+VALUES (10, 6, 7, 1,5,'nice');
+
+
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (1, 4, 2, 'F',5.43,'u','2018-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (2, 2, 3, 'F',8.43,'u','2018-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (3, 1, 1, 'C',3.83,'u','2018-11-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (4, 6, 3, 'F',28.43,'u','2018-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (5, 4, 8, 'C',6.53,'u','2017-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (6, 3, 5, 'F',18.23,'u','2018-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (7, 4, 6, 'C',7.4,'u','2017-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (8, 5, 1, 'C',5.43,'u','2018-10-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (9, 9, 7, 'F',7.43,'u','2018-02-12');
+INSERT INTO Shipping (Shipping_ID, OrderID, Address_ID,Shipping_Method,Shipping_Charge,Status,Shipping_Date )
+VALUES (10, 3, 2, 'F',15.93,'u','2018-10-12');
+
+
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (1, 6, 4, 5,'2018-10-12');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (2, 2, 3, 9,'2017-10-22')
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (3, 6, 4, 7,'2016-03-12');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (4, 9, 6, 5,'2017-10-28');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (5, 4, 8, 8,'2018-04-12');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (6, 3, 9, 5,'2017-10-19');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (7, 8, 10, 1,'2018-09-12');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (8, 6, 10, 4,'2018-10-02');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (9, 2, 4, 3,'2017-08-12');
+INSERT INTO Invoice (Invoice_ID, OrderID, User_ID, Address_ID,Creation_Date)
+VALUES (10, 7, 3, 2,'2018-10-20');
+
+
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (1, 2, 1, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (2, 9, 2, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (3, 8, 1, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (4, 3, 6, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (5, 6, 5, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (6, 2, 7, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (7, 8, 4, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (8, 1, 8, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (9, 5, 3, '');
+INSERT INTO Invoice_History (Invoice_History_ID, Invoice_ID, User_ID, Notes)
+VALUES (10, 3, 9, '');
+
+
+
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (1,'Tim', '122','33rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (2,'Arya', '128','83rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (3,'Tom', '2342','98rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (4,'TD', '122','89rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (5,'Tony', '22','36rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (6,'Sam', '62','27rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (7,'Mimo', '62','63rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (8,'Bob', '982','63rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (9,'Jack', '782','87rd', 'Seattle', 'WA',12049);
+INSERT INTO Address (Address_ID,Name, Add1,Add2, City, State,Zip)
+VALUES (10,'Micky', '135','3rd', 'Seattle', 'WA',12049);
+
+
+
+
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (1,'Tim', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (2,'Arya', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (3,'Tom', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (4,'TD', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (5,'Tony', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (6,'Sam', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (7,'Mimo', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (8,'Bob', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (9,'Jack', 'Sale','E');
+INSERT INTO Staff (Staff_ID,Staff_name, Department, Staff_Position)
+VALUES (10,'Micky','Sale','E');
+
+
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (1, 'I like it.', 1, 1, 'Good',8, 7);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (2, 'The product is exceptional!', 1, 3, 'Execellent',8, 6);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (3, 'Average experience', 5, 1, 'Neutral', 8, 3);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (4, 'Not good, need improvement.', 1, 1, 'Bad', 6, 4);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (5, 'It is above my expectation.', 2, 4, 'Good', 4, 2);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (6, 'Love it! Highly recommend!', 1, 5, 'Excellent', 8, 2);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (7, 'Not getting what I want.', 2, 1, 'Bad', 7, 10);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (8, 'Not much to comment', 1, 8, 'Neutral', 8, 9);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (9, 'Overall good experience', 5, 1, 'Good', 6, 8);
+INSERT INTO Customer_Feedback (Cus_Fb_ID, Comment, User_ID, Item_ID, Response, Session_ID, Staff_ID)
+VALUES (10, 'Brilliant! It is the best thing I have ever got.', 6, 7, 'Excellent', 5, 1);
+
+
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (1, 'Adult Dresses 20 percent off', 1, 'Discount', 2);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (2, 'End of the Summer Sale', 1, 'Seasonal Promotion', 3)
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (3, 'Clothing Black Friday Sale', 4, 'Holiday Promotion', 3);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (4, 'Christmas Jacket Sale', 2, 'Holiday Promotion', 8);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (5, 'Children Boots 30 percent off', 10, 'Discount', 7);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (6, 'Clothing Black Friday Sale', 6, 'Holiday Promotion', 5);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (7, 'Women Gloves 40 percent off', 8, 'Discount', 8);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (8, 'New Year Sweater Sale', 5, 'Holiday Promotion', 9);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (9, 'End of the Winter Sale', 5, 'Seasonal Promotion', 10);
+INSERT INTO Advertising (Ad_ID, Title, Item_ID, Type, Staff_ID)
+VALUES (10, 'Furniture 15 percent off', 13, 'Discount', 7);
+
+
+
+
+
---
+++ OnlineShoppingTables.sql
@@ -0,0 +1,407 @@
+/*
+Group 8
+
+
+Database: OnlineShoppingMall
+*/
+
+
+create database OnlineShoppingMall
+USE OnlineShoppingMall;
+GO
+
+
+CREATE TABLE User_Account(
+User_id INT NOT NULL PRIMARY KEY,
+user_name VARCHAR(45) NOT NULL,
+password VARCHAR(45) NOT NULL,
+time_stamp DATETIME,
+trans_number INT);
+
+
+CREATE TABLE User_Session(
+Session_id INT NOT NULL PRIMARY KEY,
+IP_address VARCHAR(225) NOT NULL,
+clicks INT,
+time_stamp DATETIME,
+User_id INT NOT NULL 
+	REFERENCES dbo.User_Account(User_id)
+);
+
+
+CREATE TABLE Inventory_Item(
+item_id INT IDENTITY NOT NULL PRIMARY KEY,
+price MONEY NOT NULL,
+item_name VARCHAR(45) NOT NULL,
+qty INT,
+catalog_id INT,
+vendor_id INT 
+);
+
+
+CREATE TABLE Shopping_Cart(
+cart_id INT NOT NULL PRIMARY KEY,
+active BIT,
+total_price MONEY,
+User_id INT NOT NULL 
+	REFERENCES User_Account(User_id)
+);
+
+
+CREATE TABLE Cart_Detail(
+cart_detial_id INT NOT NULL PRIMARY KEY,
+item_id INT NOT NULL
+	REFERENCES Inventory_Item(item_id),
+qty INT,
+time_stamp DATETIME,
+card_id INT NOT NULL
+	REFERENCES Shopping_Cart(cart_id));
+
+CREATE TABLE Vendor(
+vendor_id INT NOT NULL PRIMARY KEY,
+VendorName VARCHAR(45) NOT NULL,
+VendorAddress VARCHAR(45) NOT NULL);
+
+
+CREATE TABLE InventoryCatalog(
+catalog_id INT NOT NULL PRIMARY KEY,
+CatalogName VARCHAR(45) NOT NULL);
+
+ALTER TABLE Inventory_Item
+ADD CONSTRAINT PK1_Catalog FOREIGN KEY (catalog_id)
+REFERENCES InventoryCatalog(catalog_id);
+
+ALTER TABLE Inventory_Item
+ADD CONSTRAINT PK2_Vendor FOREIGN KEY (vendor_id)
+REFERENCES Vendor(vendor_id);
+
+
+
+DROP TABLE OrderHeader
+CREATE TABLE OrderHeader
+(
+	OrderID INT IDENTITY,
+	OrderDate DATETIME,
+	TotalAmount decimal(10,2) NOT NULL,
+	Status VARCHAR(255) NOT NULL,
+	UserID INT NOT NULL,
+	Discount decimal(10,2),
+	Tax decimal(10,2),
+	PRIMARY KEY (OrderID),
+	FOREIGN KEY (UserID) REFERENCES User_Account(User_id)
+)
+
+ALTER TABLE OrderHeader ALTER COLUMN Discount decimal NOT NULL
+ALTER TABLE OrderHeader ALTER COLUMN Tax decimal NOT NULL
+
+
+CREATE FUNCTION FU_TotalDue(@OrderID INT)
+RETURNS DECIMAL
+AS
+BEGIN
+	DECLARE @TotalDue DECIMAL;
+	SELECT @TotalDue = (TotalAmount + Tax - Discount)
+	FROM OrderHeader
+	WHERE OrderID = @OrderID
+	RETURN @TotalDue
+END
+
+ALTER TABLE OrderHeader
+DROP COLUMN TotalDue
+
+ALTER TABLE OrderHeader
+ADD TotalDue AS (dbo.FU_TotalDue(OrderID));
+
+
+DROP TABLE CreditCard
+CREATE TABLE CreditCard
+(
+	CardID BIGINT,
+	HolderName VARCHAR(255) NOT NULL,
+	ExpireDate DATE,
+	UserID INT NOT NULL,
+	PRIMARY KEY (CardID),
+	FOREIGN KEY (UserID) REFERENCES User_Account(User_id)
+)
+
+DROP TABLE Payment
+CREATE TABLE Payment
+(
+	PaymentID INT IDENTITY,
+	TotalAmount decimal(10,2) NOT NULL,
+	Status VARCHAR(255) NOT NULL,
+	PayTime TIMESTAMP,
+	CardID BIGINT,
+	OrderID INT,
+	PRIMARY KEY (PaymentID),
+	FOREIGN KEY (OrderID) REFERENCES OrderHeader(OrderID),
+	FOREIGN KEY (CardID) REFERENCES CreditCard(CardID)
+)
+
+ALTER TABLE Payment ALTER COLUMN OrderID INT NOT NULL
+
+DROP FUNCTION FU_CheckTotalAmount
+CREATE FUNCTION FU_CheckTotalAmount (@OrderID INT)
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @flag BIT = 1;
+	IF (
+	SELECT ISNULL(SUM(TotalAmount),0) FROM Payment WHERE @OrderID = OrderID
+	) <= (SELECT TotalDue FROM OrderHeader WHERE @OrderID = OrderID)
+	BEGIN
+		SELECT @flag = 0		
+	END
+	RETURN @flag
+END
+
+ALTER TABLE Payment DROP CONSTRAINT MaxTotalAmount
+ALTER TABLE Payment ADD CONSTRAINT MaxTotalAmount CHECK (dbo.FU_CheckTotalAmount(OrderID) = 0);
+
+CREATE TABLE OrderItem
+(
+	OrderItemID INT IDENTITY,
+	ItemID INT NOT NULL,
+	qty INT NOT NULL,
+	OrderID INT NOT NULL,
+	PRIMARY KEY (OrderItemID),
+	FOREIGN KEY (OrderID) REFERENCES OrderHeader(OrderID),
+	FOREIGN KEY (ItemID) REFERENCES Inventory_Item(item_id)
+)
+
+CREATE TABLE OrderItemHistory
+(
+	OrderHistoryID INT IDENTITY,
+	OrderItemID INT NOT NULL,
+	qty INT NOT NULL,
+	Status VARCHAR(255) NOT NULL,
+	Note VARCHAR(255),
+	StatusTime TIMESTAMP,
+	PRIMARY KEY (OrderHistoryID),
+	FOREIGN KEY (OrderItemID) REFERENCES OrderItem(OrderItemID)
+)
+
+CREATE TRIGGER TR_OrderHis
+ON OrderItem
+AFTER INSERT
+AS
+BEGIN 
+	DECLARE @OrderItemID INT
+	DECLARE @qty INT
+	SELECT @OrderItemID = OrderItemID FROM Inserted
+	SELECT @qty =  qty FROM Inserted
+	INSERT INTO OrderItemHistory
+	(OrderItemID,qty,Status,Note)
+	VALUES
+	(@OrderItemID,@qty,'purchased','')
+END
+
+
+CREATE TABLE Customer_Service
+(
+Cus_Ser_ID int  PRIMARY KEY,
+User_ID int FOREIGN KEY REFERENCES User_Account(User_ID) NOT NULL,
+Item_ID int FOREIGN KEY REFERENCES Inventory_Item(Item_ID) NOT NULL,
+Staff_ID int FOREIGN KEY REFERENCES Staff(Staff_ID) NOT NULL,
+Session_ID int FOREIGN KEY REFERENCES User_Session(Session_ID) NOT NULL,
+Notes varchar(225)
+)
+
+
+CREATE TABLE Shipping
+(
+Shipping_ID int  PRIMARY KEY,
+OrderID int FOREIGN KEY REFERENCES OrderHeader(OrderID) NOT NULL,
+Address_ID int FOREIGN KEY REFERENCES Address(Address_ID) NOT NULL,
+Shipping_Method varchar NOT NULL,
+Shipping_Charge decimal(5,2) NOT NULL,
+Status varchar NOT NULL,
+Shipping_Date date 
+)
+
+
+CREATE TABLE Invoice 
+(
+Invoice_ID int  PRIMARY KEY,
+OrderID int FOREIGN KEY REFERENCES OrderHeader(OrderID) NOT NULL,
+User_ID int FOREIGN KEY REFERENCES User_Account(User_ID) NOT NULL,
+Address_ID int FOREIGN KEY REFERENCES Address(Address_ID) NOT NULL,
+Creation_Date date
+)
+
+
+CREATE TABLE Invoice_History 
+(
+Invoice_History_ID int PRIMARY KEY,
+Invoice_ID int FOREIGN KEY REFERENCES Invoice(Invoice_ID) NOT NULL,
+User_ID int FOREIGN KEY REFERENCES User_Account(User_ID) NOT NULL,
+Notes varchar(225),
+Invoice_History_Time timestamp NOT NULL
+)
+
+
+CREATE TABLE Address 
+(
+Address_ID int PRIMARY KEY,
+Name varchar(45) NOT NULL,
+Add1 varchar(45) NOT NULL,
+Add2 varchar(45),
+City varchar(45) NOT NULL,
+State varchar(45) NOT NULL,
+Zip int NOT NULL
+);
+
+
+CREATE TABLE Staff
+(
+Staff_ID int  PRIMARY KEY,
+Staff_name varchar(45) not NULL,
+Department varchar(45) not NULL,
+Staff_Position varchar(45) not NULL
+);
+
+
+CREATE TABLE Customer_Feedback
+(
+Cus_Fb_ID int PRIMARY KEY,
+Comment VARCHAR(255),
+User_ID int FOREIGN KEY REFERENCES User_Account(User_ID) NOT NULL,
+Item_ID int FOREIGN KEY REFERENCES Inventory_Item(Item_ID) NOT NULL,
+Response VARCHAR(45),
+Session_ID int FOREIGN KEY REFERENCES User_Session(Session_ID) NOT NULL,
+Staff_ID int FOREIGN KEY REFERENCES Staff(Staff_ID) NOT NULL
+);
+
+
+CREATE TABLE Advertising 
+(
+Ad_ID int PRIMARY KEY,
+Title VARCHAR(45) NOT NULL,
+Item_ID int FOREIGN KEY REFERENCES Inventory_Item(Item_ID) NOT NULL,
+Type VARCHAR(45),
+Staff_ID int FOREIGN KEY REFERENCES Staff(Staff_ID) NOT NULL
+);
+
+
+-- Create views
+DROP VIEW CustomerPurchase_vw
+CREATE VIEW CustomerPurchase_vw
+AS
+SELECT ua.User_id UserID, user_name,ISNULL(YEAR(OrderDate),0) PurchaseYear,ISNULL(SUM(TotalDue),0) TotalDue
+FROM OrderHeader o
+FULL JOIN User_Account ua
+ON ua.User_id = o.UserID
+GROUP BY CUBE(ua.User_id),user_name,YEAR(OrderDate)
+
+DROP VIEW ItemPurchase_vw
+CREATE VIEW ItemPurchase_vw
+AS
+SELECT ii.item_id ItemID, item_name,price,ISNULL(YEAR(OrderDate),0) PurchaseYear,ISNULL(SUM(TotalDue),0) TotalDue
+FROM OrderItem oi
+FULL JOIN Inventory_Item ii
+ON ii.item_id = oi.ItemID
+FULL JOIN OrderHeader oh
+ON oi.OrderID = oh.OrderID
+GROUP BY CUBE(ii.item_id),item_name,price,YEAR(OrderDate)
+
+
+--Encryption column
+CREATE TABLE TempUserAccount
+(
+UseID int PRIMARY KEY,
+UserName nvarchar(50),
+UserPassword nvarchar(50),
+EncUserName varbinary(200),
+EncPassword varbinary(200)
+);
+
+-- Create DMK
+CREATE MASTER KEY
+ENCRYPTION BY PASSWORD = 'Test_Password';
+
+-- Create certificate to protect symmetric key
+CREATE CERTIFICATE Certificate01
+WITH SUBJECT = 'OnlineShopping Test Certificate',
+EXPIRY_DATE = '2026-10-31';
+
+-- Create symmetric key to encrypt data
+CREATE SYMMETRIC KEY SymmetricKey01
+WITH ALGORITHM = AES_128
+ENCRYPTION BY CERTIFICATE Certificate01;
+
+-- Open symmetric key
+OPEN SYMMETRIC KEY SymmetricKey01
+DECRYPTION BY CERTIFICATE Certificate01;
+
+/* Populate temp table with 100 encrypted names from the Person.Person table */
+
+INSERT
+INTO TempUserAccount
+(
+UseID,
+UserName,
+UserPassword,
+EncUserName,
+EncPassword
+)
+SELECT User_id, user_name, password, 
+EncryptByKey(Key_GUID(N'SymmetricKey01'), user_name),
+EncryptByKey(Key_GUID(N'SymmetricKey01'), password)
+FROM OnlineShoppingMall.dbo.User_Account
+ORDER BY User_id;
+
+-- Update the temp table with decrypted names
+UPDATE TempUserAccount
+SET UserName = DecryptByKey(EncUserName),
+UserPassword = DecryptByKey(EncPassword);
+
+-- Show the results
+SELECT UseID,
+UserName,
+UserPassword,
+EncUserName,
+EncPassword
+FROM TempUserAccount;
+
+--Drop table 
+DROP TABLE TempUserAccount;
+
+-- Close the symmetric key
+CLOSE SYMMETRIC KEY SymmetricKey01;
+
+-- Drop the symmetric key
+DROP SYMMETRIC KEY SymmetricKey01;
+
+-- Drop the certificate
+DROP CERTIFICATE Certificate01;
+
+--Drop the DMK
+DROP MASTER KEY;
+
+
+
+
+
+--Trigger on Cart_Detail for updating inventory amount 
+CREATE TRIGGER UpdateInventoryAmount
+ON Cart_Detail
+FOR INSERT, UPDATE, DELETE
+AS
+BEGIN
+	UPDATE Inventory_Item
+	SET qty -= (ISNULL(i.qty,0))
+	FROM Inventory_Item v, inserted i
+	WHERE v.item_id = i.item_id;
+	UPDATE Inventory_Item
+	SET qty += (ISNULL(d.qty,0))
+	FROM Inventory_Item v, deleted d
+	WHERE v.item_id = d.item_id;
+END;
+
+DROP TRIGGER UpdateInventoryAmount
+
+select * from  Shopping_Cart;
+select * from Inventory_Item;
+select * from Cart_Detail;
+
+
